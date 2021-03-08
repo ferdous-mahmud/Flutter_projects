45028d38e7108310b73c1dd6536401263a8ee24e
ferdous-webdev <ferdous.webdev@gmail.com>
2021-03-08 19:46:25 +0600

    first commit

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Hello World',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    ),
+  ));
+}

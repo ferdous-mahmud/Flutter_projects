09edf86288b829c9532d032fd874f96e808ac23f
ferdous-webdev <ferdous.webdev@gmail.com>
2021-04-18 22:54:56 +0600

    face invalid api key problem

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  final String apiKey = '979a625e81843868b88a359e97c97908';
+  double latitude;
+  double longitude;
+  
 
   @override
   void initState() {
@@ -18,14 +24,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async{
     Location location = Location();
     await location.getCurrentLocatin();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
   }
 
   void getData() async {
-    var url = Uri.https('api.openweathermap.org/data/2.5/weather?id=2172797&appid', '/books/v1/volumes', {'q': '{http}'});
-    var response = await http.get(url);
-    print(response);
+    var uri = Uri.https('api.openweathermap.org', 'data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    http.Response response = await http.get(uri);
+    if(response.statusCode == 200){
+        String data = response.body;
+
+        var decodedData = jsonDecode(data);
+
+        var lon = decodedData['coord']['lon'];
+        print('Longitude $lon');
+      }else{
+        int statusCode = response.statusCode;
+        print('Status Code => $statusCode');
+      } 
+      print(response.body);
   }
 
   @override
